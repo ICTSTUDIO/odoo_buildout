212ec14abdbe188614b3e5fa600a269d0d1b504f
Odooswiss <odoo.swiss@gmail.com>
2016-07-22 09:09:31 +0200

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,3 @@
-apt-get install git vim python2.7 postgresql nano python-virtualenv gcc python2.7-dev libxml2-dev libxslt1-dev libevent-dev libsasl2-dev libldap2-dev libpq-dev libpng12-dev libjpeg-dev node-less node-clean-css xfonts-75dpi xfonts-base
+apt-get install git vim pwgen python2.7 postgresql nano python-virtualenv gcc python2.7-dev libxml2-dev libxslt1-dev libevent-dev libsasl2-dev libldap2-dev libpq-dev libpng12-dev libjpeg-dev node-less node-clean-css xfonts-75dpi xfonts-base
 wget http://nightly.odoo.com/extra/wkhtmltox-0.12.1.2_linux-jessie-amd64.deb
 dpkg -i wkhtmltox-0.12.1.2_linux-jessie-amd64.deb
